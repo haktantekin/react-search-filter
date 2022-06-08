3d54c9d2c8b8786a29e234b7094785205cbcb0c1
haktan-tekin <webhaktan@gmail.com>
2022-06-08 14:07:31 +0300

    basic search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
-const App = () => {
-  return <div>Hello world!</div>;
-};
+import { useState } from "react";
+import "./app.css";
+import { Users } from "./users";
+
+function App() {
+  const [query, setQuery] = useState("");
+
+  return (
+    <div className="app">
+      <input
+        className="search"
+        placeholder="Search..."
+        onChange={(e) => setQuery(e.target.value)}
+      ></input>
+
+      <ul className="list">
+        {Users.filter((user) =>
+          user.first_name.toLowerCase().includes(query)
+        ).map((user) => (
+          <li key={user.id} className="listItem">
+            {user.first_name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default App;
